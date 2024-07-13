79b53423003d108f87f09d2c8850eb753b1b9dcb
Elian Renteria <elianrenteriadevelopment@gmail.com>
2024-07-13 10:35:27 -0700

    added broadcast

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, jsonify, send_from_directory
 from flask_sock import Sock
 import sqlite3
 
 app = Flask(__name__, static_folder='.')
 sock = Sock(app)
+
+clients = []
 
 def get_chat_history():
     conn = sqlite3.connect('chat.db')
@@ -19,19 +21,24 @@ def chat_history():
 
 @sock.route('/ws')
 def chat(ws):
-    while True:
-        message = ws.receive()
-        if message:
-            conn = sqlite3.connect('chat.db')
-            c = conn.cursor()
-            c.execute('INSERT INTO messages (message) VALUES (?)', (message,))
-            conn.commit()
-            conn.close()
-            ws.send(message)
+    clients.append(ws)
+    try:
+        while True:
+            message = ws.receive()
+            if message:
+                conn = sqlite3.connect('chat.db')
+                c = conn.cursor()
+                c.execute('INSERT INTO messages (message) VALUES (?)', (message,))
+                conn.commit()
+                conn.close()
+                for client in clients:
+                    client.send(message)
+    finally:
+        clients.remove(ws)
 
 @app.route('/')
 def index():
     return send_from_directory('.', 'index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
